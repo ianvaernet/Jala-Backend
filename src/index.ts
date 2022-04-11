564f08e5df8bb20c960ad1a75fc245b4e17cfe00
Ian Vaernet <ianvaernet@gmail.com>
2022-04-11 18:00:39 -0300

    Running express with ts-node

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+  response.send('Hello world!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+});
