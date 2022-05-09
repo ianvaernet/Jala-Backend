1bcea26a909e2688d7f1fdb36c7d7d5abff2cad0
Ian Vaernet <ianvaernet@gmail.com>
2022-05-09 00:02:30 -0300

    Add Rook movement

--- FinalWork-ChessAPI/src/piece/core/rook.ts
+++ FinalWork-ChessAPI/src/piece/core/rook.ts
@@ -3,6 +3,8 @@ import { Position } from '../../position';
 
 export class Rook extends Piece {
   canMove(position: Position): boolean {
-    return true;
+    const differentPosition = super.canMove(position);
+    const rookMovement = position.getFile() === this.position.getFile() || position.getRank() === this.position.getRank();
+    return differentPosition && rookMovement;
   }
 }
