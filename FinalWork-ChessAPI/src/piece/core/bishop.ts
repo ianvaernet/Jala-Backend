1eb86058af3167e3c6287e32ed5b290890c6413d
Ian Vaernet <ianvaernet@gmail.com>
2022-05-09 00:01:58 -0300

    Add bishop movement

--- FinalWork-ChessAPI/src/piece/core/bishop.ts
+++ FinalWork-ChessAPI/src/piece/core/bishop.ts
@@ -3,6 +3,10 @@ import { Position } from '../../position';
 
 export class Bishop extends Piece {
   canMove(position: Position): boolean {
-    return true;
+    const differentPosition = super.canMove(position);
+    const bishopMovement =
+      Math.abs(this.position.getRank() - position.getRank()) ===
+      Math.abs(this.position.getFile().charCodeAt(0) - position.getFile().charCodeAt(0));
+    return differentPosition && bishopMovement;
   }
 }
