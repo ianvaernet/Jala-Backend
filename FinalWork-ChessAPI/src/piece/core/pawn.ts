1531c42c35bb7c57f0e61daaafa9b3782f9d11d8
Ian Vaernet <ianvaernet@gmail.com>
2022-05-09 00:02:14 -0300

    Add Pawn movement

--- FinalWork-ChessAPI/src/piece/core/pawn.ts
+++ FinalWork-ChessAPI/src/piece/core/pawn.ts
@@ -3,6 +3,15 @@ import { Position } from '../../position';
 
 export class Pawn extends Piece {
   canMove(position: Position): boolean {
-    return true;
+    const differentPosition = super.canMove(position);
+    const isInInitialPosition =
+      (this.getColor() === 'White' && this.position.getRank() == 2) ||
+      (this.getColor() === 'Black' && this.position.getRank() == 7);
+    const forwardValue = this.getColor() === 'White' ? 1 : -1;
+    const oneStepForward = this.position.getRank() + forwardValue === position.getRank();
+    const twoStepsForward = this.position.getRank() + forwardValue * 2 === position.getRank();
+    const pawnMovement =
+      this.position.getFile() === position.getFile() && (oneStepForward || (isInInitialPosition && twoStepsForward));
+    return differentPosition && pawnMovement;
   }
 }
