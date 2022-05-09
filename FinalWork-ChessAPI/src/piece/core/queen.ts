da1949d9c48ae43d15062600345e0da7fee9ead5
Ian Vaernet <ianvaernet@gmail.com>
2022-05-09 00:02:22 -0300

    Add Queen movement

--- FinalWork-ChessAPI/src/piece/core/queen.ts
+++ FinalWork-ChessAPI/src/piece/core/queen.ts
@@ -3,11 +3,11 @@ import { Position } from '../../position';
 
 export class Queen extends Piece {
   canMove(position: Position): boolean {
-    const samePosition = position.getFile() === this.position.getFile() && position.getRank() === this.position.getRank();
-    const rook = position.getFile() === this.position.getFile() || position.getRank() === this.position.getRank();
-    const bishop =
+    const differentPosition = super.canMove(position);
+    const rookMovement = position.getFile() === this.position.getFile() || position.getRank() === this.position.getRank();
+    const bishopMovement =
       Math.abs(this.position.getRank() - position.getRank()) ===
       Math.abs(this.position.getFile().charCodeAt(0) - position.getFile().charCodeAt(0));
-    return !samePosition && (rook || bishop);
+    return differentPosition && (rookMovement || bishopMovement);
   }
 }
